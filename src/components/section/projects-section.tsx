456e13e2d17c7cacba3ae483aa06f08fbbe55cff
Sayak-Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2026-01-16 22:55:50 +0530

    helo

--- src/components/section/projects-section.tsx
+++ src/components/section/projects-section.tsx
@@ -46,7 +46,6 @@ export default function ProjectsSection() {
                                 dates={project.dates}
                                 tags={project.technologies}
                                 image={project.image}
-                                links={project.links}
                             />
                         </BlurFade>
                     ))}
